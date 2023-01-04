1ec57de7b30d97867c3cd11a131c8f858e028d97
Georgijs V <4hollow22@gmail.com>
2023-01-04 14:46:06 +0200

    working on adding list items, fixing bugs with overflow

--- script.js
+++ script.js
@@ -1,25 +1,66 @@
 class ToDoList {
-    constructor(day){
+    constructor(day, month, heading, input, bulletPoints, progressBar){
         this.day = day;
+        this.month = month;
+        this.heading = heading;
+        this.input = input;
+        this.bulletPoints = bulletPoints;
+        this.progressBar = progressBar;
     }
     
-    bulletPoints = [];
+    bulletPointsArray = [];
     doneBulletPoints = []
 
-    addBulletPoints(input){
+    addBulletPoints(){
         // get info from the input
+        const bulletPointText = this.input.value;
         // add that info to the bulletpoint array
+        this.bulletPointsArray.push(bulletPointText);
+        // append div element with checkbox and bulletpoint text to the main bullet poin div
+        this.bulletPoints.appendChild(this.createBulletEntry(bulletPointText));
+
+        //console.log(this.bulletPointsArray);
     }
 
-    displayBulletPoints(){
-        // display bulletPoint array 
+    createBulletEntry(input){
+        //define all elements for bulletpoint entry
+        const entry = document.createElement('div');
+        const checkbox = document.createElement("input");
+        const span = document.createElement('span');
+        const deleteBtn = document.createElement('input');
+
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.className = "bullet_checkbox";
+
+        deleteBtn.setAttribute('type', 'button');
+        deleteBtn.className = "delete_btn";
+        deleteBtn.value = 'delete';
+
+        // adds event listener to the checkbox
+        checkbox.addEventListener('change', () => {
+            this.markAsDone(span);
+        })
+
+        span.textContent = input;
+        entry.appendChild(checkbox);
+        entry.appendChild(span);
+        entry.appendChild(deleteBtn);
+        
+        return entry;
     }
 
-    markAsDone() {
-        // if bulletpoint checkbox is marked -
+
+    markAsDone(target) {
         // cross out the entry and add it to the 
+        target.classList.toggle("checked");
+
+        this.doneBulletPoints.push(target.textContent);
+        this.bulletPointsArray.pop(target.textContent);
+        console.log("all bullet points: " + this.bulletPointsArray);
+        console.log("done bullet points: " + this.doneBulletPoints);
+
         // doneBulletPoints array
-    }
+        }
 
     updateProgressBar(){
         // consider total amount of bulletpoints to calculate 100%
@@ -54,7 +95,10 @@ class Calendar {
 }
 
 let currentDate = new Date();
-let currentMonth = currentDate.getMonth() + 1;// gets in number from 0
+
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+let currentMonth = currentDate.getMonth();// gets in number from 0
 let currentDay = currentDate.getDate(); 
 
 let todoHeading = document.querySelector("#todo_heading");
@@ -62,3 +106,15 @@ let entry = document.querySelector("#entry");
 let addBtn = document.querySelector("#add_btn");
 let bulletPoints = document.querySelector("#bullet_points");
 let progressBar = document.querySelector("#progress_bar");
+let checkbox = document.querySelector(".bullet_checkbox");
+
+
+let toDolist = new ToDoList(currentDay, months[currentMonth],todoHeading,entry, bulletPoints, progressBar);
+
+addBtn.addEventListener('click', () => {
+    toDolist.addBulletPoints();
+})
+
+// checkbox.addEventListener('change', () => {
+//     toDolist.markAsDone();
+// })
