d1768ce23aa8e75c0278de3c00cd7bd970a6ba6b
Georgijs V <4hollow22@gmail.com>
2023-01-08 20:07:53 +0200

    separated css, tweaks, combined both script files + others

--- script.js
+++ script.js
@@ -154,11 +154,11 @@ class Calendar {
     }
 }
 
-let currentDate = new Date();
-let currentMonth = currentDate.getMonth();// gets in number from 0
-let currentDay = currentDate.getDate(); 
+// let currentDate = new Date();
+// let currentMonth = currentDate.getMonth();// gets in number from 0
+// let currentDay = currentDate.getDate(); 
 
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 let todoHeading = document.querySelector("#todo_heading");
 let entry = document.querySelector("#entry");
@@ -168,8 +168,9 @@ let progressBar = document.querySelector("#progress_bar");
 let bar = document.querySelector("#bar");
 let checkbox = document.querySelector(".bullet_checkbox");
 
+// months[currentMonth]
 
-let toDolist = new ToDoList(currentDay, months[currentMonth],todoHeading,entry, bulletPoints, progressBar, bar);
+let toDolist = new ToDoList(5, "January",todoHeading,entry, bulletPoints, progressBar, bar);
 
 addBtn.addEventListener('click', () => {
     toDolist.addBulletPoints();
@@ -190,3 +191,70 @@ toDolist.updateProgressBar();
 // function that clears everything
 
 //function that runs when the page is loading - gets everything from local storage and creates bulletpoints with that with that
+
+
+const currentDate = document.querySelector(".current-date");
+const daysTag = document.querySelector(".days");
+const prevNextIcon = document.querySelectorAll(".icons span");
+
+let date = new Date();
+let currYear = date.getFullYear();
+let currMonth = date.getMonth();
+
+const months = ["January", "February", "March", "April", "May", "June", "July", 
+                "August", "September", "October", "November", "December"];
+
+
+function renderCalendar() {
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDate(); // getting first day of month
+    let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(); // getting last date of month
+    let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(); // getting last day of month
+    let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+
+    let liTag = "";
+    let = isToday = "";
+
+    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+    }
+
+    for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
+        // adding active class to li if the current day month and year matched
+        if (i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear()) {
+            isToday = "active";
+        } else {
+            isToday = "";
+        }
+        liTag += `<li class="${isToday}">${i}</li>`;
+    }
+
+    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+    }
+
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
+    daysTag.innerHTML = liTag;
+}
+
+renderCalendar();
+
+prevNextIcon.forEach(icon => {
+    icon.addEventListener("click", () =>{
+        // clicked icon is previous - decrement current month, and otherwise
+        if (icon.id === "prev") {
+            currMonth -= 1;
+        } else {
+            currMonth += 1;
+        }
+        if (currMonth < 0 || currMonth > 11) { 
+            // if current month less than 0 or greater than 11 create new data of current year and month and pass it as date value
+            date = new Date(currYear, currMonth);
+            currYear = date.getFullYear(); // update current year with new date
+            currMonth = date.getMonth(); // updating current month with new date month
+        } else { // else pass the new date as date value
+            date = new Date();
+
+        }
+        renderCalendar();
+    })
+});
